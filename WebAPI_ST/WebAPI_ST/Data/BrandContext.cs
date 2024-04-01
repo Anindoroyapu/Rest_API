82c4036ad0c27774b7ca1c642437a12124abc2ba
Anindo Roy <anindoroy112@gmail.com>
2024-04-01 11:29:39 +0600

    brandContext_done

---
+++ WebAPI_ST/WebAPI_ST/Data/BrandContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_ST.Data
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<BrandContext> Brands { get; set; }
+    }
+}
