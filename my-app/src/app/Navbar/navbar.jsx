2d7581e88dfbf337c86c17f6535ab0971c2d4806
Anindo Roy <anindoroy112@gmail.com>
2024-04-06 23:12:32 +0600

    Body_update

--- my-app/src/app/Navbar/navbar.jsx
+++ my-app/src/app/Navbar/navbar.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./navbar.css";
+import dPhoto from "../../assets/desktop_version.jpg";
+import photo1 from "../../assets/111.jpg";
+import photo2 from "../../assets/222.jpg";
+import photo3 from "../../assets/333.jpg";
+import photo4 from "../../assets/444.jpg";
 
 function Navbar() {
   return (
-    <nav className=" ">
-      <div className="navbar-header">Photo</div>
+    <nav className="header">
+      <div className="navbar-header">
+        <img src={dPhoto} alt="" />
+      </div>
       <div>
         <div className="container text-center">
           <div className="searchBar">
@@ -24,9 +31,35 @@ function Navbar() {
           <div className="row">
             <div className="col ">
               <div className="desktopMenu list-group">
-                <a className="list-group-item" href="/Home">
-                  Home
-                </a>
+                <div className="dropdown">
+                  <a className="list-group-item" href="/Home">
+                    Home
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a
+                        class="btn dropdown-item dropdown-toggle"
+                        href="# "
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="# ">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="# ">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
                 <a className="list-group-item" href="/Home">
                   Bike Price
                 </a>
@@ -62,8 +95,33 @@ function Navbar() {
                 </a>
               </div>
             </div>
-            <div className="col-8">Body</div>
-            <div className="col">Body2</div>
+            <div className="col-8 border">
+              <div>
+                <div>
+                  <h1>Browse Bikes By</h1>
+                </div>
+                <button type="button" class="btn">
+                  Brand
+                </button>
+                <button type="button" class="btn">
+                  Budget
+                </button>
+                <button type="button" class="btn">
+                  Displacement
+                </button>
+                <button type="button" class="btn">
+                  Body Style
+                </button>
+              </div>
+            </div>
+            <div className="col">
+              <div>
+                <img src={photo1} alt="" />
+                <img src={photo2} alt="" />
+                <img src={photo3} alt="" />
+                <img src={photo4} alt="" />
+              </div>
+            </div>
           </div>
 
           <div className="row">
