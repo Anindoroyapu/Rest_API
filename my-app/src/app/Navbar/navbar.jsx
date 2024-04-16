abf1836e2159af40618c30ff96ad0feb80c49029
Anindo Roy <anindoroy112@gmail.com>
2024-04-16 23:57:11 +0600

    menu_file_create

--- my-app/src/app/Navbar/navbar.jsx
+++ my-app/src/app/Navbar/navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./navbar.css";
+//import Marquee from "react-marquee-slider";
 import dPhoto from "../../assets/desktop_version.jpg";
-//import photo1 from "../../assets/111.jpg";
-//import photo2 from "../../assets/222.jpg";
-//import photo3 from "../../assets/333.jpg";
-//import photo4 from "../../assets/444.jpg";
+import photo1 from "../../assets/111.jpg";
+import photo2 from "../../assets/222.jpg";
+import photo3 from "../../assets/333.jpg";
+import photo4 from "../../assets/444.jpg";
 import logo1 from "../../assets/hero.png";
 import logo2 from "../../assets/honda.png";
 import logo3 from "../../assets/suzuki.png";
@@ -13,19 +14,19 @@ import logo4 from "../../assets/tvs.png";
 import logo5 from "../../assets/yamaha.png";
 
 function Navbar() {
-  const [collapsed, setCollapsed] = useState({
-    collapse1: false,
-    collapse2: false,
-    collapse3: false,
-  });
-  const images = ["111.jpg", "222.jpg", "333.jpg", "444.jpg"];
-
-  const toggleCollapse = (collapseKey) => {
-    setCollapsed((prevState) => ({
-      ...prevState,
-      [collapseKey]: !prevState[collapseKey],
-    }));
+  const CTYPES = {
+    BRAND: "brand",
+    BUDGET: "budget",
+    DISPLACEMENT: "displacement",
+    BODYSTYLE: "bodyStyle",
   };
+
+  const [categoryName, setCategoryName] = useState(CTYPES.BRAND);
+
+  const toggleCollapse = (keyName) => {
+    setCategoryName(keyName);
+  };
+
   return (
     <nav className="header">
       <div className="navbar-header">
@@ -34,20 +35,20 @@ function Navbar() {
       <div>
         <div className="container text-center">
           <div className="searchBar">
-            <form class="d-flex p-md-4" role="search">
+            <form className="d-flex p-md-4" role="search">
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button class="btn btn-primary me-2" type="submit">
+              <button className="btn btn-primary me-2" type="submit">
                 Search
               </button>
-              <button class="btn btn-primary me-2" type=" ">
+              <button className="btn btn-primary me-2" type=" ">
                 Login
               </button>
-              <button class="btn btn-outline-primary me-2" type=" ">
+              <button className="btn btn-outline-primary me-2" type=" ">
                 SignUp
               </button>
             </form>
@@ -59,10 +60,10 @@ function Navbar() {
                   <a className="list-group-item" href="/Home">
                     Home
                   </a>
-                  <ul class="dropdown-menu">
+                  <ul className="dropdown-menu">
                     <li>
                       <a
-                        class="btn dropdown-item dropdown-toggle"
+                        className="btn dropdown-item dropdown-toggle"
                         href="# "
                         role="button"
                         data-bs-toggle="dropdown"
@@ -72,43 +73,43 @@ function Navbar() {
                       </a>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="# ">
+                      <a className="dropdown-item" href="# ">
                         Another action
                       </a>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="# ">
+                      <a className="dropdown-item" href="# ">
                         Something else here
                       </a>
                     </li>
                   </ul>
                 </div>
 
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Bike Price
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Compare
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="home">
                   Accessories
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Showrooms
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Brands
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Scooter
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Electric
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Upcoming
                 </a>
-                <a className="list-group-item" href="/Home">
+                <a className="list-group-item" href="/home">
                   Review
                 </a>
                 <a className="list-group-item" href="/Home">
@@ -124,21 +125,21 @@ function Navbar() {
                 <div>
                   <h1>Browse Bikes By</h1>
                 </div>
-                <div class="row border-bottom font-weight-bold">
-                  <div class="col border-primary">
+                <div className="row border-bottom font-weight-bold">
+                  <div className="col border-primary">
                     <button
                       type="button"
-                      class="btn browse-col"
-                      onClick={() => toggleCollapse("collapse1")}
+                      className="btn browse-col"
+                      onClick={() => toggleCollapse(CTYPES.BRAND)}
                     >
-                      {collapsed.collapse1 ? "Brand" : "Brand"}
+                      Brand
                     </button>
 
-                    {!collapsed.collapse1 && (
+                    {categoryName === CTYPES.BRAND && (
                       <div>
-                        <div class="container ">
-                          <div class="row d-inline-flex gap-4 me-2 p-4 border-bottom">
-                            <div class="col ">
+                        <div className="container ">
+                          <div className="row d-inline-flex gap-4 me-2 p-4 border-bottom">
+                            <div className="col ">
                               <a href="# ">
                                 <img
                                   src={logo1}
@@ -147,7 +148,7 @@ function Navbar() {
                                 />
                               </a>
                             </div>
-                            <div class="col">
+                            <div className="col">
                               <a href="# ">
                                 <img
                                   src={logo2}
@@ -156,7 +157,7 @@ function Navbar() {
                                 />
                               </a>
                             </div>
-                            <div class="col">
+                            <div className="col">
                               <a href="# ">
                                 <img
                                   src={logo3}
@@ -165,7 +166,7 @@ function Navbar() {
                                 />
                               </a>
                             </div>
-                            <div class="col">
+                            <div className="col">
                               <a href="# ">
                                 <img
                                   src={logo4}
@@ -174,7 +175,7 @@ function Navbar() {
                                 />
                               </a>
                             </div>
-                            <div class="col">
+                            <div className="col">
                               <a href="# ">
                                 <img
                                   src={logo5}
@@ -188,43 +189,43 @@ function Navbar() {
                       </div>
                     )}
                   </div>
-                  <div class="col border-primary">
+                  <div className="col border-primary">
                     <button
                       type="button"
-                      class="btn browse-col"
-                      onClick={() => toggleCollapse("collapse2")}
+                      className="btn browse-col"
+                      onClick={() => toggleCollapse(CTYPES.BUDGET)}
                     >
-                      {collapsed.collapse2 ? "Budget" : "Budget"}
+                      Budget
                     </button>
 
-                    {!collapsed.collapse2 && (
+                    {categoryName === CTYPES.BUDGET && (
                       <div>
-                        <div class="container ">
-                          <div class="row d-inline-flex gap-4 me-2 p-2">
-                            <div class="col btn-col">
+                        <div className="container ">
+                          <div className="row d-inline-flex gap-4 me-2 p-2">
+                            <div className="col btn-col">
                               <a href="# ">Under 60000</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 1Lakh</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 1.5Lakh</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 2Lakh</a>
                             </div>
                           </div>
-                          <div class="row d-inline-flex gap-4 me-2 p-4 border-bottom">
-                            <div class="col btn-col">
+                          <div className="row d-inline-flex gap-4 me-2 p-4 border-bottom">
+                            <div className="col btn-col">
                               <a href="# ">Under 2.5Lakh</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 3Lakh</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 4Lakh</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Other</a>
                             </div>
                           </div>
@@ -232,42 +233,42 @@ function Navbar() {
                       </div>
                     )}
                   </div>
-                  <div class="col border-primary">
+                  <div className="col border-primary">
                     <button
                       type="button"
-                      class="btn browse-col"
-                      onClick={() => toggleCollapse("collapse3")}
+                      className="btn browse-col"
+                      onClick={() => toggleCollapse(CTYPES.DISPLACEMENT)}
                     >
-                      {collapsed.collapse3 ? "Displacement" : "Displacement"}
+                      Displacement
                     </button>
-                    {!collapsed.collapse3 && (
+                    {categoryName === CTYPES.DISPLACEMENT && (
                       <div>
-                        <div class="container ">
-                          <div class="row d-inline-flex gap-4 me-2 p-2">
-                            <div class="col btn-col">
+                        <div className="container ">
+                          <div className="row d-inline-flex gap-4 me-2 p-2">
+                            <div className="col btn-col">
                               <a href="# ">Under 100cc</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 150cc</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 200cc</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 250cc</a>
                             </div>
                           </div>
-                          <div class="row d-inline-flex gap-4 me-2 p-4 border-bottom">
-                            <div class="col btn-col">
+                          <div className="row d-inline-flex gap-4 me-2 p-4 border-bottom">
+                            <div className="col btn-col">
                               <a href="# ">Under 300cc</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 350cc</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Under 400cc</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Other</a>
                             </div>
                           </div>
@@ -275,56 +276,56 @@ function Navbar() {
                       </div>
                     )}
                   </div>
-                  <div class="col border-primary">
+                  <div className="col border-primary">
                     <button
                       type="button"
-                      class="btn browse-col"
-                      onClick={() => toggleCollapse("collapse4")}
+                      className="btn browse-col"
+                      onClick={() => toggleCollapse(CTYPES.BODYSTYLE)}
                     >
-                      {collapsed.collapse4 ? "Body Style" : "Body Style"}
+                      Body Style
                     </button>
-                    {!collapsed.collapse4 && (
+                    {categoryName === CTYPES.BODYSTYLE && (
                       <div>
-                        <div class="container ">
-                          <div class="row d-inline-flex gap-4 me-2 p-2">
-                            <div class="col btn-col">
+                        <div className="container ">
+                          <div className="row d-inline-flex gap-4 me-2 p-2">
+                            <div className="col btn-col">
                               <a href="# ">Sports Bike</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Scooter</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Cruiser</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Commuter</a>
                             </div>
                           </div>
-                          <div class="row d-inline-flex gap-4 me-2 p-4 border-bottom">
-                            <div class="col btn-col">
+                          <div className="row d-inline-flex gap-4 me-2 p-4 border-bottom">
+                            <div className="col btn-col">
                               <a href="# ">Naked Sports</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Cafe Racer</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Off-Road</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Adventure</a>
                             </div>
                           </div>
-                          <div class="row d-inline-flex gap-4 me-2 p-4 border-bottom">
-                            <div class="col btn-col">
+                          <div className="row d-inline-flex gap-4 me-2 p-4 border-bottom">
+                            <div className="col btn-col">
                               <a href="# ">Moped</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Tourer</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Mini bike</a>
                             </div>
-                            <div class="col btn-col">
+                            <div className="col btn-col">
                               <a href="# ">Other</a>
                             </div>
                           </div>
@@ -338,10 +339,11 @@ function Navbar() {
               </div>
             </div>
             <div className="col">
-              <div className="marquee-container">
-                {images.map((image, index) => (
-                  <img key={index} src={image} alt={`Slide ${index + 1}`} />
-                ))}
+              <div className="">
+                <img src={photo1} alt="" />
+                {/*    <img src={photo2} alt="" />
+                <img src={photo3} alt="" />
+                  <img src={photo4} alt="" />    */}
               </div>
             </div>
           </div>
