4477f628582716e760e8a865357c29b7450d073f
Anindo Roy <anindoroy112@gmail.com>
2024-04-18 10:48:35 +0600

    App_update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./app/Home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BikePrice from "./app/Pages/BikePrice/bikePrice";
+import Accessories from "./app/Pages/Accessories/accessories";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route index element={<Home />} />
-
-        <Route path="/BikePrice" element={<BikePrice />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/bike-price" element={<BikePrice />} />
+        <Route path="/accessories" element={<Accessories />} />
       </Routes>
     </BrowserRouter>
   );
