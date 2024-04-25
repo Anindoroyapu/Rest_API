ab3b4e7e93f1d5b2eb2730b379ba427308d07d60
Anindo Roy <anindoroy112@gmail.com>
2024-04-25 10:59:33 +0600

    Agency_File_done

---
+++ WebAPI_ST/WebAPI_ST/Models/AgencyProfile/AgencyProfile.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI_ST.Models.AgencyProfile
+{
+    [Table("AgencyProfile", Schema = "public")]
+    public class AgencyProfile
+    {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string AgencyName { get; set; } = "";
+        public int OwnerUserId { get; set; } = 0;
+
+        public int Creator { get; set; } = 0;
+        public long TimeCreated { get; set; } = TimeOperation.GetUnixTime();
+        public long TimeUpdated { get; set; } = TimeOperation.GetUnixTime();
+
+
+
+    }
+}
---
+++ WebAPI_ST/WebAPI_ST/Utils/TimeOperation.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace WebAPI_ST.Utils
+{
+    public class TimeOperation
+    {
+        public static long GetUnixTime()
+        {
+            return (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
+        public static long GetUnixTime(DateTime dateTime)
+        {
+            return (int)dateTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+
+        public static long GetUnixTime(string? dateString)
+        {
+            if (dateString == null)
+            {
+                return 0;
+            }
+
+            return (int)DateTime.ParseExact(dateString, "yyyy-MM-dd", CultureInfo.InvariantCulture).Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+
+        public static int GetDay(long unixTimeStamp)
+        {
+            return (int)(new DateTime(1970, 1, 1).AddSeconds(unixTimeStamp)).Day;
+        }
+    }
+}
